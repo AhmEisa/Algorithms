bd6dabfb9ea2f2ce181657a7dece330ec8cfcf73
ahmedeisa <ahmed92eisa@gmail.com>
2018-02-03 15:17:55 +0200

    develop stack

---
+++ Algorithms/DoublyLinkedList.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Signle chain of nodes
+/// Head Pointer and Tail Pointer
+/// operations : Add , Remove , Find , Enumerate
+/// </summary>
+namespace Algorithms
+{
+    public class DoublyLinkedListNode<T>
+    {
+        public DoublyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+        public DoublyLinkedListNode<T> Next { get; set; }
+        public DoublyLinkedListNode<T> Previous { get; set; }
+
+    }
+    public class DoublyLinkedLists<T> : ICollection<T>
+    {
+        public DoublyLinkedListNode<T> Head { get; private set; }
+        public DoublyLinkedListNode<T> Tail { get; private set; }
+        public void AddFirst(T value)
+        {
+            AddFirst(new DoublyLinkedListNode<T>(value));
+        }
+        public void AddLast(T value)
+        {
+            AddLast(new DoublyLinkedListNode<T>(value));
+        }
+
+
+        #region Add
+        public void AddFirst(DoublyLinkedListNode<T> node)
+        {
+            DoublyLinkedListNode<T> temp = Head;
+            Head = node;
+            Head.Next = temp;            
+            if (Count == 1) { Tail = Head; }
+            else
+            {
+                temp.Previous = Head;
+            }
+            Count++;
+        }
+        public void AddLast(DoublyLinkedListNode<T> node)
+        {
+            if (Count == 0)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+                node.Previous = Tail;
+            }
+            Tail = node;
+            Count++;
+        }
+        #endregion Add
+
+        #region Remove
+        public void RemoveLast()
+        {
+            if (Count != 0)
+            {
+                if (Count == 1) { Head = null; Tail = null; }
+                else
+                {
+                    Tail.Previous.Next = null;
+                    Tail = Tail.Previous;
+                }
+                Count--;
+            }
+        }
+        public void RemoveFirst()
+        {
+            if (Count != 0)
+            {
+                Head = Head.Next;
+                Count--;
+            }
+            if (Count == 0) { Tail = null; }
+            else
+            {
+                Head.Previous = null;
+            }
+        }
+        #endregion Remove
+
+        #region Collection Implementaion Methods
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+        public bool Contains(T item)
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item)) { return true; }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            DoublyLinkedListNode<T> previous = null;
+            DoublyLinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null) { Tail = previous; }
+                        else
+                        {
+                            current.Next.Previous = previous;
+                        }
+                        Count--;
+                    }
+                    else
+                    {
+                        RemoveFirst();
+                    }
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        #endregion Collection Implementaion Methods
+    }
+
+}
+
---
+++ Algorithms/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// A last in first out collection implemented as a lined list 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _List = new LinkedList<T>();
+        public void Push(T item) {
+            _List.AddFirst(item);
+        }
+        public T Pop()
+        {
+            if (_List.Count == 0) { throw new InvalidOperationException("The Stack is Empty"); }
+            T value = _List.First.Value;
+            _List.RemoveFirst();
+            return value;
+        }
+        public T Peek()
+        {
+            if (_List.Count == 0) { throw new InvalidOperationException("The Stack is Empty"); }
+            return _List.First.Value;            
+        }
+        public int Count
+        {
+            get
+            {
+                return _List.Count;
+            }
+        }
+        public void Clear()
+        {
+            _List.Clear();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _List.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _List.GetEnumerator();
+        }
+    }
+}
