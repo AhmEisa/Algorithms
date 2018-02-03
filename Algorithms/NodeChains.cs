04e7fd8f68b4ba748681b5a288a9a48159c676a5
ahmedeisa <ahmed92eisa@gmail.com>
2018-02-03 13:16:32 +0200

    develop chain nodes

---
+++ Algorithms/NodeChains.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+    }
+    class NodeChains
+    {
+        static void Main(string[] args)
+        {
+            Node first = new Node { Value = 3 };
+            Node middle = new Node { Value = 5 };
+            first.Next = middle;
+            Node last = new Node { Value = 7 };
+            middle.Next = last;
+
+            PrintList(first);
+        }
+        private static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
